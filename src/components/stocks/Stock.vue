55734773182f1dc2548565e494a174a4eacde721
AdamMarczyk <adam.marczyk90@gmail.com>
2018-06-11 18:16:39 +0000

    create stock components

--- src/components/stocks/Stock.vue
+++ src/components/stocks/Stock.vue
@@ -1,3 +1,29 @@
 <template>
-  <h1>The Stock Component</h1>
+  <div class="col-sm-6 col-md-4">
+    <div class="panel panel-success">
+      <div class="panel-heading">
+        <h3 class="panel-title">
+          NAME
+          <small>(Price: PRICE)</small>
+          </h3>
+      </div>
+      <div class="panel-">
+        <div class="pull-left">
+          <input
+            type="number"
+            class="form-control"
+            placeholder="Quantity"
+          >
+        </div>
+        <div class="pull-right">
+          <button
+            class="btn btn-succes"
+          >
+            Buy
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+
 </template>
