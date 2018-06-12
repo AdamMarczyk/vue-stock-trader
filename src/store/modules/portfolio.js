cdf07decdd05ca2f593ac143aa4a50034975a161
AdamMarczyk <adam.marczyk90@gmail.com>
2018-06-12 17:45:57 +0000

    add buy and sale mutations

---
+++ src/store/modules/portfolio.js
@@ -0,0 +1,28 @@
+const state = {
+  funds: 10000,
+  stocks: []
+};
+
+const mutations = {
+  BUY_STOCK(state, { stockId, quantity, stockPrice }) {
+    const record = state.stocks.find(element => element.id == stockId);
+    if (record) {
+      record.quantity += quantity;
+    } else {
+      state.stocks.push({
+        id: stockId,
+        quantity
+      });
+    }
+    state.funds -= stockPrice * quantity;
+  },
+  SELL_STOCK(state, { stockId, quantity, stockPrice }) {
+    const record = state.stocks.find(element => element.id == stockId);
+    if (record.quantity > quantity) {
+      record.quantity -= quantity;
+    } else {
+      state.stocks.splice(state.stocks.indexOf(record), 1);
+    }
+    state.funds += stockPrice * quantity;
+  }
+};
