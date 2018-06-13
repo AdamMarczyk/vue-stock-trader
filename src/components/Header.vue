dac75e0c7965aea9deba630650bd5f30430930a1
AdamMarczyk <adam.marczyk90@gmail.com>
2018-06-13 21:51:22 +0200

    implement save data

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -28,7 +28,7 @@
             Save & Load <span class="caret"></span>
           </a>
           <ul class="dropdown-menu">
-            <li><a href="#">Save Data</a></li>
+            <li><a href="#" @click="saveData">Save Data</a></li>
             <li><a href="#">Load Data</a></li>
           </ul>
         </li>
@@ -57,6 +57,14 @@
       ]),
       endDay() {
         this.randomizeStocks();
+      },
+      saveData() {
+        const data = {
+          funds: this.$store.getters.funds,
+          stockPortfolio: this.$store.getters.stockPortfolio,
+          stocks: this.$store.getters.stocks,
+        };
+        this.$http.put('data.json', data);
       }
     }
   }
